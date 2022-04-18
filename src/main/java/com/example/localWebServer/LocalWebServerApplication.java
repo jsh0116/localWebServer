867d7c17c36129fcd6654f8ee4bd4e2749fd99c8
seunghoon_jeong <seunghoon_jeong@tmax.co.kr>
2022-04-19 00:25:26 +0900

    Initial commit

---
+++ src/main/java/com/example/localWebServer/LocalWebServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.localWebServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalWebServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalWebServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/localWebServer/LocalWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.localWebServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalWebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
