151e68bff6e3073951b158404e9bc8311b58b131
seunghoon_jeong <seunghoon_jeong@tmax.co.kr>
2022-04-20 17:34:46 +0900

    infoServer 와 내부 통신하여 job 정보를 요청하도록 변경 - WebClient 를 통해 InfoServer 에 요청 - InfoServer 로부터 받은 Response Body 내용을 Mono 로 변환 - HashMap 형태로 전달하도록 설정

--- src/main/java/com/example/localWebServer/handler/PersonHandler.java
+++ src/main/java/com/example/localWebServer/handler/PersonHandler.java
@@ -1,29 +1,28 @@
 package com.example.localWebServer.handler;
 
-import com.example.localWebServer.model.Person;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.util.HashMap;
 import java.util.Optional;
 
 @Component
 public class PersonHandler {
-
+    WebClient webClient = WebClient.create("http://localhost:8761");
     public Mono<ServerResponse> getResponseByName(ServerRequest request) {
         // get first query parameter
         Optional<String> query =  request.queryParam("name");
 
         if (query.isPresent()) {
             String name = query.get();
-            Person person = new Person(name, "Hello " + name);
-            return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).bodyValue(person);
+//            Person person = new Person(name, "Hello " + name);
+            Mono<HashMap> result = webClient.get().uri("/job?name=" + name).retrieve().bodyToMono(HashMap.class);
+            return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromProducer(result, HashMap.class));
         } else {
             return ServerResponse.badRequest().build();
         }
